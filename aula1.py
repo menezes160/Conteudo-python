63edb6d18cfcd05ea4185d7abc5262c6558dfad4
ARTHUR MENEZES DA SILVA <arthur_silva164@estudante.sesisenai.org.br>
2025-10-02 16:49:22 -0300

    conteudo soobre print e variaveis

---
+++ aula1.py
@@ -0,0 +1,12 @@
+#como nosso primeiro código, vamos escapar da maldição
+print('Olá mundo')
+#Abaixo está o comando print com sep e end padrão
+print("Lógica", "de" ,"progamação",sep=" ",end="\n")
+#Abaixo cada cotactação é um enter (\n siguinifica enter)
+print("vai" , "imprimir" , "em" ,  "baixo?",sep="\n")
+#Abaixo de cada comando possui o end alterado para impriir na emsma linha
+print("vai",end="")
+print("imprimir",end="")
+print("na",end="")
+print("mesma",end="")
+print("linha",end="")      
